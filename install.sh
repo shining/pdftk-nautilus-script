ea6176535e99846a522d3f58ebb7cd2c793e9066
Xavier Chantry <xavier.chantry@intersec.com>
2025-01-15 11:24:26 +0100

    add pdf_to_png script

--- install.sh
+++ install.sh
@@ -8,10 +8,12 @@ REPO="${PWD}"
 install() {
     cd "${1}" || return
     echo "installing Merge PDF and Burst PDF in ${1}"
-    unlink "Merge PDF" || true
+    unlink "Merge PDF" &> /dev/null || true
     cp "${REPO}"/merge_pdf "Merge PDF"
-    unlink "Burst PDF" || true
+    unlink "Burst PDF" &> /dev/null || true
     cp "${REPO}"/burst_pdf "Burst PDF"
+    unlink "PDF to PNG" &> /dev/null || true
+    cp "${REPO}"/pdf_to_png "PDF to PNG"
 }
 
 install "${HOME}/.local/share/nautilus/scripts"
